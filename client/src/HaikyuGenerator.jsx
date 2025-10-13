a8c1e76bcff9f64f199e5111adb13480754e442d
Anthony Gallegos <galleg46@uwm.edu>
2025-10-13 15:04:36 -0500

    whitespace

--- client/src/HaikyuGenerator.jsx
+++ client/src/HaikyuGenerator.jsx
@@ -11,6 +11,7 @@ import Paper from '@mui/material/Paper';
 import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 
+
 function HaikyuGenerator() {
     const columnsConfig = [
         { id: 'character-1', label: 'Character 1', type: 'teams' },
